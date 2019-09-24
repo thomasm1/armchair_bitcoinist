8e6a9ac318bf314ef424691d4e37c739f30aa87e
thomas maestas <thomasm1.maestas@gmail.com>
2019-09-24 17:12:49 -0600

    flask app enva_bitcoinBuyer with bitcoin calculator started

---
+++ README.md
@@ -0,0 +1,80 @@
+## Armchair Bitcoinist
+#### Python Flask Application
+_Thomas Maestas_
+| Fx | Tools | URLS |
+| ------ |------ | ------ |
+| LIB | Flask (WerkZeug Toolkit) | [Flask]  | 
+| UI | JINJA2 template engine |  [Jinja2] | 
+| DATAVIZ | Matplotlib (low-level) |  [Matplotlib]  |
+| DATAVIZ | Seaborn (high-level) |  [Seaborn] |
+| DATA | NumPy |  [NumPy] |
+| DATA | Pandas | [Pandas]  |
+| DATA-AUTH | requests oath library | [requests:oathlib]  |  
+| DATA-UTIL | requests |  [requests]  | 
+| DATA-SCRAPERS | beautiful soup | [bs4]  | 
+| CLI-SDK |  boto3 (AWS) |   [AWS SDK]  | 
+
+##### Instructions:
+```sh
+$ set FLASK_APP=enva_bitcoinBuyer
+$ flask run
+``` 
+* boto3             1.9.233
+* botocore          1.12.233
+* certifi           2019.3.9
+* chardet           3.0.4
+* Click             7.0
+* cycler            0.10.0
+* Cython            0.29.13 
+* docutils          0.15.2
+* Flask             1.0.3
+* idna              2.8
+* itsdangerous      1.1.0
+* Jinja2            2.10.1
+* jmespath          0.9.4
+* kiwisolver        1.1.0
+* MarkupSafe        1.1.1
+* matplotlib        3.1.1
+* mysqlclient       1.3.12
+* numpy             1.17.0
+* oauthlib          3.0.1
+* pandas            0.25.0
+* pip               19.1.1
+* pyparsing         2.4.2
+* python-dateutil   2.8.0
+* pytz              2019.2
+* requests          2.22.0
+* requests-oauthlib 1.2.0
+* s3transfer        0.2.1
+* scipy             1.3.0
+* seaborn           0.9.0
+* setuptools        41.0.1
+* six               1.12.0
+* sqlparse          0.3.0
+* twython           3.7.0
+* urllib3           1.25.3
+* Werkzeug          0.15.4
+* wheel             0.33.4''' 
+ 
+### Todos
+
+ - Too much to count!! 
+
+License
+----
+
+MIT
+
+** Software, :-)**
+[Flask]: <http://flask.palletsprojects.com/en/1.1.x/>
+   [Jinja2]: <https://jinja.palletsprojects.com/en/2.10.x/>
+   [Matplotlib]: <https://matplotlib.org/>
+   [Seaborn]: <https://seaborn.pydata.org/>
+   [NumPy]: <https://numpy.org/>
+   [Pandas]: <https://pandas.pydata.org/>
+   [requests]: <https://2.python-requests.org/en/master/> 
+   [requests:oathlib]:<https://github.com/requests/requests-oauthlib>
+   [bs4]: <https://www.crummy.com/software/BeautifulSoup/bs4/doc/>
+   [AWS SDK]: <https://aws.amazon.com/sdk-for-python/>
+   
+   Author website: [thomasmaestas.net](https://thomasmaestas.net)
---
+++ enva_bitcoinBuyer/__init__.py
@@ -0,0 +1,90 @@
+from flask import flash
+
+from flask import Flask, request, render_template
+
+app = Flask(__name__)
+members = []
+
+@app.route('/')
+def index():
+    return render_template("home.jinja2")
+    # return "Console"
+
+
+@app.route('/home', methods=['GET', 'POST'])
+def home():
+        if request.method == 'POST':
+                btc = float(request.form['btc'])
+                profit = float(request.form['profit'])
+
+                print(btc)
+                print(profit)
+
+                a = float(profit) / 100
+                print(a)
+
+                b = 1.005012531 * float(btc)
+                print(float(b))
+
+                s = float(1 + a) * float(b)
+                print("%.16f" % s)
+
+                flash('BTC. ' + "%.16f" % s, 'success')
+        return render_template("home.jinja2")
+    # "ArmChair Bitcoinist"
+
+
+@app.route('/home/console')
+def console():  
+    return render_template("console.jinja2")
+
+
+@app.route('/home/messages')
+def messages():
+    return render_template("messages.jinja2")
+
+
+@app.route('/home/<input>')
+def input(input):
+    return "Input: {} <br /><br /><a href='/home'>back</a>".format(input)
+
+
+@app.route('/home/<input>/<message>')
+def home_input_message(input, message):
+    return render_template("home_input_message.jinja2",
+                           input=input,
+                           message=message)
+    # return "{0}, {1}".format(name, message)
+
+
+@app.route('/register')
+def register():
+    return render_template("register.jinja2")
+
+
+@app.route('/registrants')
+def registrants():
+    return render_template("registered.html", members=members)
+
+
+@app.route('/registerPost')  # , methods=["POST"])
+def registerPost():
+    members = []
+    name = request.args.get("name")
+    gender = request.args.get("gender")
+    if not name or not gender:
+        return render_template("failure.jinja2")
+    members.append(f"{name} with gender {gender}")
+    return redirect("/registrants")
+
+# URL: http://localhost:5000/query?a=test&b=123
+@app.route('/query')
+def query():
+    a = request.args.get('a')
+    b = request.args.get('b')
+    return "<h4>Variable a = {0}<br />while Variable b = {1}</h4><a href='/home'>back</a>".format(a, b)
+ 
+
+if __name__ == '__main__':
+    app.secret_key = 'xxxxxxx' # ../xx/www/.nogit
+    app.run(debug=True)
